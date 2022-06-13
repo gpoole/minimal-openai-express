847bf8c7d7af896b9e5ac0241477ebcb27b00cc5
Greg Poole <greg.poole@protonmail.com>
2022-06-13 20:46:47 +1000

    Talk like a horse

--- api/talk.mjs
+++ api/talk.mjs
@@ -7,9 +7,16 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const handle = async (req, res) => {
   try {
     const openai = new OpenAI(OPENAI_API_KEY);
+
+    const prompt = `
+      You are a chat bot. You are also a horse. You need to talk a bit like a horse but in an opera.
+
+      User: ${req.body.prompt}
+      You:`;
+
     const gptResponse = await openai.complete({
       engine: 'davinci',
-      prompt: req.body.prompt,
+      prompt,
       maxTokens: 256,
       // temperature: 0.9,
       // topP: 1,
@@ -17,6 +24,7 @@ const handle = async (req, res) => {
       // frequencyPenalty: 0,
       // bestOf: 1,
       // n: 1,
+      stop: ['\n', "You:", "User:"],
       stream: false,
     });
 
